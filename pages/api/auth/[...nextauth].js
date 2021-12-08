f0c4113bc176ac4572fd534073f924deddb855fc
Evan Prenger <eprenger23@gmail.com>
2021-12-08 15:17:03 -0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,22 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers/Google";
+import Providers from "next-auth/providers";
+import { FirebaseAdapter } from "@next-auth/firebase-adapter";
 import { db } from "../../../firebase";
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
   adapter: FirebaseAdapter(db),
+
+  //   NOTE: If you ever need the users ID, this is how you obtain it in the session!
+  callbacks: {
+    session: async (session, user) => {
+      session.id = user.id;
+      return Promise.resolve(session);
+    },
+  },
 });

8a2d753b781981652b683fca1c6ed4b500c5b572
Evan Prenger <eprenger23@gmail.com>
2021-12-08 23:08:24 -0500

    Update [id].js

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -1,4 +1,4 @@
-//import TextEditor from "../../components/TextEditor";
+import TextEditor from "../../components/TextEditor";
 import Button from "@material-tailwind/react/Button";
 import Icon from "@material-tailwind/react/Icon";
 import { useRouter } from "next/dist/client/router";
@@ -8,28 +8,31 @@ import { getSession, signOut, useSession } from "next-auth/client";
 import Login from "../../components/Login";
 
 export default function Doc() {
-  const [session] = useSession();
+  const [session, loading] = useSession();
+  console.log(session);
   if (!session) return <Login />;
+
+  const router = useRouter();
+  const { id } = router.query;
   const [snapshot, loadingSnapshot] = useDocumentOnce(
     db.collection("userDocs").doc(session.user.email).collection("docs").doc(id)
   );
-  const router = useRouter();
-  const { id } = router.query;
 
-  if (!loadingSnapshot && snapshot?.data()?.fileName) {
+  // Redirect if user tries to access a URL they do not have access to...
+  if (!loadingSnapshot && !snapshot?.data()?.fileName) {
+    // Filename will not be present if the user doesnt have access...
     router.replace("/");
   }
 
   return (
     <div>
       <header className="flex justify-between items-center p-3 pb-1">
-        <span onClick={() => router.push("/")}>
+        <span onClick={() => router.push("/")} className="cursor-pointer">
           <Icon name="description" size="5xl" color="blue" />
         </span>
-
-        <div className="flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-600">
-          <h2>{snapshot?.data()?.fileName}</h2>
-          <div>
+        <div className="flex-grow px-2">
+          <h2 className="text-lg text-left">{snapshot?.data()?.fileName}</h2>
+          <div className="flex items-center text-sm -ml-1 h-8 text-gray-600 space-x-1">
             <p className="option">File</p>
             <p className="option">Edit</p>
             <p className="option">View</p>
@@ -38,7 +41,38 @@ export default function Doc() {
             <p className="option">Tools</p>
           </div>
         </div>
+
+        <Button
+          color="lightBlue"
+          buttonType="filled"
+          size="regular"
+          className="hidden md:!inline-flex h-10"
+          rounded={false}
+          block={false}
+          iconOnly={false}
+          ripple="light"
+        >
+          <Icon name="people" size="md" /> SHARE
+        </Button>
+
+        <img
+          onClick={signOut}
+          className="cursor-pointer h-10 w-10 rounded-full ml-2"
+          src={session.user.image}
+          alt=""
+        />
       </header>
+      <TextEditor />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }

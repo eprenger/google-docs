3be363b01f31f33c1ca8d110c28beba74947ce7e
Evan Prenger <eprenger23@gmail.com>
2021-12-08 23:04:18 -0500

    updates

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -9,14 +9,35 @@ import Login from "../../components/Login";
 
 export default function Doc() {
   const [session] = useSession();
-  const router = useRouter();
   if (!session) return <Login />;
+  const [snapshot, loadingSnapshot] = useDocumentOnce(
+    db.collection("userDocs").doc(session.user.email).collection("docs").doc(id)
+  );
+  const router = useRouter();
+  const { id } = router.query;
+
+  if (!loadingSnapshot && snapshot?.data()?.fileName) {
+    router.replace("/");
+  }
+
   return (
     <div>
-      <header className="flex justify-between items-center p-3">
+      <header className="flex justify-between items-center p-3 pb-1">
         <span onClick={() => router.push("/")}>
           <Icon name="description" size="5xl" color="blue" />
         </span>
+
+        <div className="flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-600">
+          <h2>{snapshot?.data()?.fileName}</h2>
+          <div>
+            <p className="option">File</p>
+            <p className="option">Edit</p>
+            <p className="option">View</p>
+            <p className="option">Insert</p>
+            <p className="option">Format</p>
+            <p className="option">Tools</p>
+          </div>
+        </div>
       </header>
     </div>
   );
